d3dd86abc6be57c6d7b09bb1f848528229631947
dannykrug <dannykrug@gmail.com>
2019-01-14 13:13:16 -0500

    added framework files

--- src/App.js
+++ src/App.js
@@ -1,10 +1,25 @@
 import React, { Component } from 'react';
 import { View, Text } from 'react-native';
+import firebase from 'firebase';
+import { Header } from './components/common';
+
 
 class App extends Component {
+  componentWillMount() {
+    firebase.initializeApp({
+        apiKey: 'AIzaSyAg_DMasCgMZkOwisIhSMd_7bGGBEyZddE',
+        authDomain: 'authentication-4d7b9.firebaseapp.com',
+        databaseURL: 'https://authentication-4d7b9.firebaseio.com',
+        projectId: 'authentication-4d7b9',
+        storageBucket: 'authentication-4d7b9.appspot.com',
+        messagingSenderId: '928435007444'
+      });
+  }
+
   render() {
     return (
       <View>
+        <Header headerText="Authentication" />
         <Text>An App</Text>
       </View>
     );
