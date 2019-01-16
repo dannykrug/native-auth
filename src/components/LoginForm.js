3cff02135f33990898824c36a21bfbfed11f2339
dannykrug <dannykrug@gmail.com>
2019-01-16 18:04:05 -0500

    added spinner file

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -1,24 +1,55 @@
 import React, { Component } from 'react';
-import { TextInput } from 'react-native';
-import { Button, Card, CardSection } from './common';
+import { Text } from 'react-native';
+import firebase from 'firebase';
+import { Button, Card, CardSection, Input } from './common';
 
 class LoginForm extends Component {
-  state = { text: '' };
+  state = { email: '',
+            password: '',
+            error: '' };
+
+  onButtonPress() {
+    const { email, password } = this.state;
+
+    this.setState({ error: '' });
+
+    firebase.auth().signInWithEmailAndPassword(email, password)
+    .catch(() => {
+      firebase.auth().createUserWithEmailAndPassword(email, password)
+      .catch(() => {
+        this.setState({ error: 'Authentication Failed.' });
+      });
+    });
+  }
 
   render() {
     return (
       <Card>
         <CardSection>
-          <TextInput
-          value={this.state.text}
-          onChangeText={text => this.setState({ text })}
-          style={{ height: 20, width: 100 }}
+          <Input
+            placeholder="user@gmail.com"
+            label='Email'
+            value={this.state.email}
+            onChangeText={email => this.setState({ email })}
           />
         </CardSection>
-        <CardSection />
 
         <CardSection>
-          <Button>
+          <Input
+            secureTextEntry
+            placeholder="password"
+            label="Password"
+            value={this.state.password}
+            onChangeText={password => this.setState({ password })}
+          />
+        </CardSection>
+
+        <Text style={styles.errorTextStyle}>
+          {this.state.error}
+        </Text>
+
+        <CardSection>
+          <Button onPress={this.onButtonPress.bind(this)}>
             Log in
           </Button>
         </CardSection>
@@ -26,5 +57,13 @@ class LoginForm extends Component {
     );
   }
 }
+
+const styles = {
+  errorTextStyle: {
+    fontSize: 20,
+    alignSelf: 'center',
+    color: 'red'
+  }
+};
 
 export default LoginForm;
