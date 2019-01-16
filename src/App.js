1443f7ae6a4e12a3dbf247b75b330f21913ec771
dannykrug <dannykrug@gmail.com>
2019-01-16 18:57:46 -0500

    app loading spinner

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
 import { View } from 'react-native';
 import firebase from 'firebase';
-import { Header } from './components/common';
+import { Header, Button, Spinner } from './components/common';
 import LoginForm from './components/LoginForm';
 
 
 class App extends Component {
+  state = { loggedIn: null };
+
   componentWillMount() {
     firebase.initializeApp({
         apiKey: 'AIzaSyAg_DMasCgMZkOwisIhSMd_7bGGBEyZddE',
@@ -15,13 +17,32 @@ class App extends Component {
         storageBucket: 'authentication-4d7b9.appspot.com',
         messagingSenderId: '928435007444'
       });
+
+      firebase.auth().onAuthStateChanged((user) => {
+        if (user) {
+          this.setState({ loggedIn: true });
+        } else {
+          this.setState({ loggedIn: false });
+        }
+      });
+  }
+
+  renderContent() {
+    switch (this.state.loggedIn) {
+      case true:
+      return <Button>Log Out</Button>;
+      case false:
+        return <LoginForm />;
+      default:
+        return <Spinner size='large' />;
+    }
   }
 
   render() {
     return (
       <View>
         <Header headerText="Authentication" />
-        <LoginForm />
+        {this.renderContent()}
       </View>
     );
   }
